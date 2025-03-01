3879321a4ef30f1d100d0def95a349d6de79a587
liudmylasovetovs <milkaegik@gmail.com>
2025-03-01 15:55:00 -0800

    versel

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 // import { store } from "./store/store";
 // import Header from "./components/Header/Header";
-import App from "./App";
+import App from "./App.jsx";
 import './index.css';
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Куди буде збережений білд
+    target: "esnext", // Використовує найновіший JS для сучасних браузерів
+    rollupOptions: {
+      output: {
+        format: "es", // Забезпечує правильний формат модулів
+      },
+    },
+  },
+});
