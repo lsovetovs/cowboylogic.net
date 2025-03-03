fef14ba549f86754eb8780d7cfc8350521200788
itservice-cl <itservice@cowboylogic.net>
2025-03-02 22:31:18 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/staging/",
   plugins: [react()],
   build: {
     outDir: "dist", // Куди буде збережений білд
