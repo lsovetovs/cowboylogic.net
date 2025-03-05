207142b39373a2cc0f628f1a066424313999eaf0
liudmylasovetovs <milkaegik@gmail.com>
2025-03-04 19:38:56 -0800

    creating Hero components for  strategies and publishing

---
+++ src/components/HeroPublishing/HeroPublishing.jsx
@@ -0,0 +1,13 @@
+import SliderPublishing from "../SliderPublishing/SliderPublishing";
+import styles from "./HeroPublishig.module.css";
+
+const HeroPublishing = () => {
+  return (
+    <section className={styles.hero}>
+      <SliderPublishing />
+   
+    </section>
+  );
+};
+
+export default HeroPublishing;
