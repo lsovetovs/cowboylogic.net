43faf85e45b46bdf9efc5b4f6283601b244e666e
liudmylasovetovs <milkaegik@gmail.com>
2025-03-04 19:52:40 -0800

    book store  adding

---
+++ src/pages/BookStore/BookStore.jsx
@@ -0,0 +1,13 @@
+import styles from './BookStore.module.css';
+import Hero from '../../components/Hero/Hero';
+
+
+const BookStore = () => {
+  return (
+    <div className={styles.container}>
+      <Hero title="BookStore" />
+    </div>
+  );
+};
+
+export default BookStore;
