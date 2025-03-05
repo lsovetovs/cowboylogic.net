924eb845bbf8b19f6bf030935e137bed18eab2b0
liudmylasovetovs <milkaegik@gmail.com>
2025-03-04 20:41:02 -0800

    make-up bookstore page

--- src/pages/BookStore/BookStore.jsx
+++ src/pages/BookStore/BookStore.jsx
@@ -1,11 +1,11 @@
 import styles from './BookStore.module.css';
-import Hero from '../../components/Hero/Hero';
+// import Hero from '../../components/Hero/Hero';
 
 
 const BookStore = () => {
   return (
     <div className={styles.container}>
-      <Hero title="BookStore" />
+      {/* <Hero title="BookStore" /> */}
     </div>
   );
 };
