094c45b9a0b748234d7ed1f463b47c2913dcfed9
liudmylasovetovs <milkaegik@gmail.com>
2025-04-17 20:12:08 -0700

    second client part

---
+++ client/vite.config.js
@@ -0,0 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  // base: "/staging/",
+  base: "/",
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Куди буде збережений білд
+    target: "esnext", // Використовує найновіший JS для сучасних браузерів
+    rollupOptions: {
+      output: {
+        format: "es", // Забезпечує правильний формат модулів
+      },
+    },
+  },
+});
