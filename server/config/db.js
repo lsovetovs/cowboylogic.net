33babc135ec7123882effbfa7864f9ca45ef841e
liudmylasovetovs <milkaegik@gmail.com>
2025-04-20 20:47:29 -0700

    connect new database mysql

--- server/config/db.js
+++ server/config/db.js
@@ -3,27 +3,26 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: process.env.DATABASE_DIALECT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
-  dialectOptions: {
-      ssl: true,
+export const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT || 3306,
+    dialect: "mysql",
+    logging: false, // можна true для дебагу
   }
-});
+);
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ PostgreSQL connected via Sequelize");
+    console.log("✅ MySQL connected via Sequelize");
   } catch (error) {
     console.error("❌ Unable to connect to DB:", error);
     process.exit(1);
   }
 };
 
-export { sequelize };
 export default connectDB;
