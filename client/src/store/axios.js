dd259306f37e21bb7e8a54772516178a2ddaa81b
liudmylasovetovs <milkaegik@gmail.com>
2025-05-21 10:06:21 -0700

    minor changes3

--- client/src/store/axios.js
+++ client/src/store/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:5000/api", 
-  baseURL: "http://clpit.duckdns.org:64660/api",
+  // baseURL: "http://localhost:5000/api",
+  // baseURL: "http://clpit.duckdns.org:64660/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: false,
 });
 
