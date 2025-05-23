cac27f6c99423e1463d2dcf3c6789f7d60e47c2a
liudmylasovetovs <milkaegik@gmail.com>
2025-05-23 12:14:00 -0700

    fix: minor import problem3

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,5 @@
 
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import ResetPasswordForm from "../../components/ResetPasswordForm/ResetPasswordForm";
 
 const ProfilePage = () => {
